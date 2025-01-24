8c8943a3838f7b1e1ac0be3ca17d2bbb4f84e4a7
Farhan4456789 <farhanzoera148@gmail.com>
2025-01-24 08:49:54 +0700

    first commit

---
+++ pertemuan2/index.php
@@ -0,0 +1,23 @@
+<?php //$nama = "Muhammad"; 
+
+//echo "Hallo Nama Saya Adalah $nama";
+
+
+//$x = 10;
+///$y = 20;
+
+//echo $x - $y;
+
+//$nama_depan = "Muhammad Farhan";
+//$nama_belakang = "Fadlil";
+
+//echo $nama_depan . " " .$nama_belakang;
+
+// var_dump(1 == "1");
+
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0);
+
+
+
+?>
